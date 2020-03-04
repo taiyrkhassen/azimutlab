42e1e2d0847663598a5896ec48871ea299bfb76c
Taiyr Khassen <tair.h49@gmail.com>
2020-03-04 12:26:18 +0600

    adapter ready

--- app/src/main/java/com/example/azimutlab/adapters/DataListAdapter.kt
+++ app/src/main/java/com/example/azimutlab/adapters/DataListAdapter.kt
@@ -1,5 +1,47 @@
 package com.example.azimutlab.adapters
 
-class DataListAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.azimutlab.R
+import com.example.azimutlab.mvp.models.PostModel
+import kotlinx.android.synthetic.main.data_list_item.view.*
+
+class DataListAdapter : RecyclerView.Adapter<DataListAdapter.ViewHolder>() {
+
+    private var dataList = ArrayList<PostModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemView =LayoutInflater.from(parent.context)
+            .inflate(R.layout.data_list_item, parent, false)
+        return ViewHolder(itemView)
+    }
+
+
+    override fun getItemCount(): Int = dataList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val post = dataList[position]
+        holder.bind(post)
+    }
+
+
+    fun addDataList(data: ArrayList<PostModel>) {
+        dataList.addAll(data)
+    }
+
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(item: PostModel){
+            itemView.apply {
+                user_id.text = item.userId.toString()
+                title.text = item.title
+                post_id.text = item.id.toString()
+                content.text = item.body
+            }
+        }
+
+    }
 
 }
