df3132815795bd32add4b06336f45f18479db432
taiyrkhassen <tair.h49@gmail.com>
2020-03-16 14:43:53 +0600

    viewModel

--- app/src/main/java/com/example/azimutlab/dagger/components/ServiceComponent.kt
+++ app/src/main/java/com/example/azimutlab/dagger/components/ServiceComponent.kt
@@ -5,11 +5,13 @@ import com.example.azimutlab.dagger.modules.RepositoryModules
 import com.example.azimutlab.dagger.modules.ServiceModule
 import com.example.azimutlab.mvvm.repository.MainRepositoryImpl
 import com.example.azimutlab.mvvm.view.activities.MainActivity
+import com.example.azimutlab.mvvm.viewmodels.MainViewModel
 import dagger.Component
 
 @Component(modules = [ServiceModule::class, RepositoryModules::class], dependencies = [AppComponent::class])
 interface ServiceComponent {
     fun inject(repository: MainRepositoryImpl)
     fun inject(activity: MainActivity)
+    fun inject(mainViewModel: MainViewModel)
     fun getApiService():ApiService
 }
--- app/src/main/java/com/example/azimutlab/mvvm/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/azimutlab/mvvm/viewmodels/MainViewModel.kt
@@ -1,6 +1,22 @@
 package com.example.azimutlab.mvvm.viewmodels
 
-class MainViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.azimutlab.AzimutApp
+import com.example.azimutlab.dagger.components.DaggerServiceComponent
+import com.example.azimutlab.mvvm.repository.MainRepositoryImpl
+import javax.inject.Inject
+
+//reference to repository
+class MainViewModel: ViewModel() {
+    init {
+        DaggerServiceComponent.builder()
+            .appComponent(AzimutApp.appComponent)
+            .build()
+            .inject(this)
+    }
+
+    @Inject
+    lateinit var mainRepos: MainRepositoryImpl
 
 
 
